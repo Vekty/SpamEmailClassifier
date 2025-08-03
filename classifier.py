e739fbec359639eb12397fb18c7f67d56f3b7131
Virlan Victor-Stefan <roxorvicecity@gmail.com>
2025-08-03 18:06:20 +0300

    Made a list of email instances

--- classifier.py
+++ classifier.py
@@ -11,26 +11,21 @@ test_data_path='/home/victor/Documents/SpamClassifier/test_data'
 training_spam_path=os.path.join(training_data_path,'spam')
 training_ham_path=os.path.join(training_data_path,'ham')
 
-def get_names(type_:str='spam')->list[str]:
-    path = os.path.join(training_spam_path, 'spam_names.txt')
-    if type=='ham':
-        path= os.path.join(training_ham_path,'ham_names.txt')
-    with open(path, 'r') as f:
-        text=f.read().split(' ')
-    return text
-
-class MailPipeline:
-    def __init__(self):
-        self.__spam_names=get_names('spam')
-        self.__ham_names=get_names('ham')
-        self.table=pd.DataFrame()
-    def read_email(self):
-        pars=parser.BytesParser(policy=policy.default)
-        with open(os.path.join(training_spam_path,self.__spam_names[0]),'rb') as f:
-            content=pars.parse(f)
+def read_email(type_:str='spam',file_name:str=None):
+    path=training_spam_path
+    if type_=='ham':
+        path=training_ham_path
+    with open(os.path.join(path,file_name),'rb') as f:
+        return parser.BytesParser(policy=policy.default).parse(f)
 
 def main():
-   mp=MailPipeline()
-   mp.read_email()
+
+  ham_names = [name for name in sorted(os.listdir(training_ham_path))]   #list of file names
+  spam_names = [name for name in sorted(os.listdir(training_spam_path))]
+
+  spam_emails=[read_email('spam',name) for name in spam_names]  #list of email instances
+  ham_emails=[read_email('ham',name) for name in ham_names]
+
+
 if __name__=='__main__':
     main()
