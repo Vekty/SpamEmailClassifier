c7cbf06942ad7e261622c992577991664e076c99
Virlan Victor-Stefan <roxorvicecity@gmail.com>
2025-08-16 16:19:01 +0300

    done, also automated download process

--- classifier.py
+++ classifier.py
@@ -1,11 +1,12 @@
 import numpy as np
 import os
 import re
-import copy
 from bs4 import BeautifulSoup
 from email import parser
 from email import policy
 from email.message import EmailMessage
+import urllib
+import tarfile
 from sklearn.model_selection import train_test_split
 from collections import Counter
 from urlextract import URLExtract
@@ -13,23 +14,44 @@ from sklearn.base import BaseEstimator,TransformerMixin
 from scipy.sparse import csr_matrix
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import cross_val_score
 from nltk import PorterStemmer
+from sklearn.metrics import recall_score,precision_score
+from sklearn.model_selection import GridSearchCV
 
-training_data_path='/home/victor/Documents/SpamClassifier/training_data'
-training_spam_path=os.path.join(training_data_path,'spam')
-training_ham_path=os.path.join(training_data_path,'ham')
+DOWNLOAD_ROOT = "http://spamassassin.apache.org/old/publiccorpus/"
+HAM_URL = DOWNLOAD_ROOT + "20030228_easy_ham.tar.bz2"
+SPAM_URL = DOWNLOAD_ROOT + "20030228_spam.tar.bz2"
+SPAM_PATH = os.path.join("datasets", "spam")
+HAM_DIR = os.path.join(SPAM_PATH, "easy_ham")
+SPAM_DIR = os.path.join(SPAM_PATH, "spam")
+
+def fetch_spam_data(ham_url=HAM_URL, spam_url=SPAM_URL, spam_path=SPAM_PATH):
+    if not os.path.isdir(spam_path):
+        os.makedirs(spam_path)
+    for filename, url in (("ham.tar.bz2", ham_url), ("spam.tar.bz2", spam_url)):
+        path = os.path.join(spam_path, filename)
+        if not os.path.isfile(path):
+            urllib.request.urlretrieve(url, path)
+        tar_bz2_file = tarfile.open(path)
+        tar_bz2_file.extractall(path=spam_path)
+        tar_bz2_file.close()
 
 def read_email(type_:str='spam',file_name:str=None)->EmailMessage:
-    path=training_spam_path
+    path=SPAM_DIR
     if type_=='ham':
-        path=training_ham_path
+        path=HAM_DIR
     with open(os.path.join(path,file_name),'rb') as f:
         return parser.BytesParser(policy=policy.default).parse(f)
 
 def html_to_plain_text(html):
     html=BeautifulSoup(html,'html.parser')
+    for a_tag in html.find_all('a'):
+        a_tag.replace_with('HYPERLINK')  #all <a> tags are replaced with the word HYPERLINK
     return html.get_text()
+
+def remove_empty_data(y,indexes):
+    y=np.delete(y,indexes)
+    return y
 
 def email_to_text(email):
     html = None
@@ -56,6 +78,7 @@ class EmailToWordCounter(BaseEstimator,TransformerMixin):
     def fit(self,X,y=None):
         return self
     def transform(self,X):
+        self.count=[]
         for x in X:
                 content=email_to_text(x)
                 if content is not None:
@@ -72,17 +95,20 @@ class EmailToWordCounter(BaseEstimator,TransformerMixin):
                         stemmed_word_counts[stemmed_word]+=count
                     word_counts=stemmed_word_counts
                     self.count.append(word_counts)
+
         return np.array(self.count)
 
 class AttributeAdder(BaseEstimator,TransformerMixin):
-        def __init__(self):
-            self.word_list=[]
+        def __init__(self,vocabulary_size=2000):
             self.vocabulary_={}
+            self.vocabulary_size=vocabulary_size
         def fit(self,X,y=None):
-            for x in X:
-                self.word_list = self.word_list + list(x.keys())
-            self.word_list = sorted(set(self.word_list))
-            self.vocabulary_= {word: index + 1 for index, word in enumerate(self.word_list)}
+            total_count = Counter()
+            for word_count in X:
+                for word, count in word_count.items():
+                    total_count[word] += count
+            most_common = total_count.most_common()[:self.vocabulary_size]
+            self.vocabulary_= {word: index + 1 for index, (word,count) in enumerate(most_common)}
             return self
         def transform(self,X):
             data = []
@@ -92,12 +118,12 @@ class AttributeAdder(BaseEstimator,TransformerMixin):
                 for word, count in x.items():
                     rows.append(row)
                     data.append(count)
-                    cols.append(self.vocabulary_.get(word))
-            return csr_matrix((data, (rows, cols)), shape=(len(X), len(self.word_list) + 1))
+                    cols.append(self.vocabulary_.get(word,0))
+            return csr_matrix((data, (rows, cols)), shape=(len(X), self.vocabulary_size + 1))
 
 def main():
-  ham_names = [name for name in sorted(os.listdir(training_ham_path))]   #list of file names
-  spam_names = [name for name in sorted(os.listdir(training_spam_path))]
+  ham_names = [name for name in sorted(os.listdir(HAM_DIR))]   #list of file names
+  spam_names = [name for name in sorted(os.listdir(SPAM_DIR))]
 
   spam_emails=[read_email('spam',name) for name in spam_names]  #list of email instances
   ham_emails=[read_email('ham',name) for name in ham_names]
@@ -106,15 +132,21 @@ def main():
   y = np.array([0] * len(ham_emails) + [1] * len(spam_emails))
 
   X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
-  X_train_copy=copy.deepcopy(X_train)
+
   mail_pipeline=Pipeline([('EmailTransformer',EmailToWordCounter()),('AttributeAdder',AttributeAdder())])
-  X_train_copy=mail_pipeline.fit_transform(X_train_copy)
-  y_train=y_train[:len(y_train)-1]
+
+  X_train=mail_pipeline.fit_transform(X_train)
+  X_test=mail_pipeline.transform(X_test)
+  y_train=np.delete(y_train,1047) #deleted element at index 1047 because for some reason the 1047th element in X is
+                                      # of type None which caused performance to severely drop
+
 
   model=LogisticRegression(max_iter=2000,random_state=42)
+  model.fit(X_train,y_train)
+  y_hat=model.predict(X_test)
 
-  score = cross_val_score(model, X_train_copy, y_train, cv=3, verbose=3)
-  print(score.mean())
+  print(recall_score(y_test,y_hat)) #97% recall
+  print(precision_score(y_test,y_hat)) #95% precision
 
 if __name__=='__main__':
     main()
